e951ff4ba65d0a89fe3ee2b7097d466fffa746cf
Trinesh Chetty <trinesh@trinstel.com>
2022-04-28 23:05:39 +0200

    working on deployment

--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
-
-
-
 const streamMonitorService = require("./services/stream.monitor.service")
 
 const streamMonitorController = require("./controllers/stream.monitor.controller")(streamMonitorService)
-
-
 
 module.exports = {
     streamMonitorController
